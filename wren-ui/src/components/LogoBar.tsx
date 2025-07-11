610a9fd90b3f03162d5e15450a6f08a15cd90e1e
Rifan <38419265+rifankurnia@users.noreply.github.com>
2025-07-11 15:34:50 +0700

    Initial commit

--- wren-ui/src/components/LogoBar.tsx
+++ wren-ui/src/components/LogoBar.tsx
@@ -1,12 +1,3 @@
-import Image from 'next/image';
-
 export default function LogoBar() {
-  return (
-    <Image
-      src="/images/logo-white-with-text.svg"
-      alt="Wren AI"
-      width={125}
-      height={30}
-    />
-  );
+  return null;
 }
